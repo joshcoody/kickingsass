1f73014f7ab9673e3f9ff2701a79355563fd8f39
Josh Coody <joshua.n.coody@gmail.com>
2016-03-15 10:58:24 -0500

    Fixing indentation issues on Step1 gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -1,39 +1,39 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		json_server: {
-			dist: {
-				options: {
-	            	hostname: 'localhost',
-					port: 5555,
-	            	db: 'db.json'
-				}
-			}
-		},
-		compass: {
-			dist: {
-				options: {
-					sassDir: 'source/sass/',
-					cssDir: 'public/css',
-					outputStyle: 'compressed',
-					require: [
-						'compass-normalize'
-					]
-    			}
-			}
-		},
-		watch: {
-			css: {
-				files: 'source/sass/**/*.scss',
-				tasks: ['compass']
-			},
-			options: {
-				"spawn": true
-			}
-		}
-	});
-	grunt.loadNpmTasks('grunt-json-server');
-	grunt.loadNpmTasks('grunt-contrib-compass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['watch']);
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    json_server: {
+      dist: {
+        options: {
+          hostname: 'localhost',
+          port: 5555,
+          db: 'db.json'
+        }
+      }
+    },
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'source/sass/',
+          cssDir: 'public/css',
+          outputStyle: 'compressed',
+          require: [
+            'compass-normalize'
+          ]
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: 'source/sass/**/*.scss',
+        tasks: ['compass']
+      },
+      options: {
+        "spawn": true
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-json-server');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['watch']);
 };
